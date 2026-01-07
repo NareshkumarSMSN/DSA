8d0b99de97d7c83dfdbce494d4f3e32f81f4904d
Naresh Kumar S <130232000+NareshkumarSMSN@users.noreply.github.com>
2026-01-07 23:49:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.38%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.c
@@ -0,0 +1,11 @@
+bool hasAlternatingBits(int n) {
+    int prev = n&1;
+    n >>= 1;
+    while(n){
+        int curr = n&1;
+        if(prev == curr) return false;
+        prev = curr;
+        n >>= 1;
+    }
+    return true;
+}
